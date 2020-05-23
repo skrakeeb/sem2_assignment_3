c73ec76c1a88cb31786019428bca2d3efd5360d1
skrakeeb <57462022+skrakeeb@users.noreply.github.com>
2020-05-23 12:08:21 +0530

    Add files via upload

---
+++ prob2.c
@@ -0,0 +1,74 @@
+/* coded by: rakeeb
+   coded for: Fourier transform of sinc function using FFTW3 library 
+*/
+
+#include <stdio.h>    
+#include <stdlib.h>
+#include <math.h>
+#include <fftw3.h>
+
+// defining sinc function
+float f( float x)
+{
+	if( x != 0)
+	{
+		return sin(x)/x ;
+	}
+	else
+	{
+		return 1 ;
+	}	
+}
+
+void main()
+{
+	int numpoints = 256;
+	float x_min= -50, x_max= 50, dx = (x_max-x_min)/(numpoints-1) , k ;
+	float x_arr[numpoints], y_ft_absolute;
+	fftw_complex y[numpoints], y_ft[numpoints];
+	fftw_plan p;
+	
+	// sampling the function y = f(x)
+	int i;
+	for(i=0; i<numpoints; i++)
+	{
+		x_arr[i] = x_min + i*dx;
+		y[i][0] = f(x_arr[i]); y[i][1]=0.0;  //making the y array           
+	}	
+    
+    //computing fft using fftw
+    p = fftw_plan_dft_1d(numpoints, y, y_ft, FFTW_FORWARD, FFTW_ESTIMATE);             	
+	fftw_execute(p);
+
+
+    //data storing 
+    FILE *data;
+    data = fopen("prob2.txt","w");
+    fprintf(data, "# k, ft_real, ft_img, ft_absolute. delimeter = ' '\n");
+
+	for(i=0; i<numpoints; i++)
+	{
+		if(i<=(numpoints/2-1))   //making the k values
+		{
+			k = 2*M_PI*i/(numpoints*dx);        
+		}
+		else
+		{
+			k = 2*M_PI*(i-numpoints)/(numpoints*dx);
+	    }
+        
+        // actual values of DFT when x_min != 0
+		y_ft[i][0] = dx*sqrt(1/(2*M_PI)) * (cos(k*x_min) * y_ft[i][0] + sin(k*x_min) * y_ft[i][1]);
+	   	y_ft[i][1] = dx*sqrt(1/(2*M_PI)) * (cos(k*x_min) * y_ft[i][1] - sin(k*x_min) * y_ft[i][0]);
+
+	    y_ft_absolute =  sqrt( pow(y_ft[i][0],2) + pow( y_ft[i][1],2) );  // absolute value of the Fourier transform
+
+	    fprintf(data, "%f %f %f %f\n", k, y_ft[i][0], y_ft[i][1], y_ft_absolute);
+	}
+
+	fclose(data);
+
+	fftw_destroy_plan(p);    
+
+
+}
---
+++ prob2.py
@@ -0,0 +1,26 @@
+#plotting of the numerical data obatined by running prob2.c
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#the analytical result
+def f(x):
+    if(abs(x)<1.0):
+        return(np.sqrt(np.pi/2.0))
+    else:
+        return(0.0)
+
+k, ft_real, ft_img , ft_abs = np.loadtxt("prob2.txt", delimiter= ' ', unpack= True, skiprows= 1)
+
+y_ft_analytic = np.zeros(k.size)
+
+for i in range(k.size):
+	y_ft_analytic[i] = f(k[i])
+
+plt.plot(k, ft_abs ,'r',label= 'by FFTW')
+plt.plot(k, y_ft_analytic,label= 'Analytic function')
+plt.legend()
+plt.xlabel('k')
+plt.ylabel('function')
+plt.title('Plot of Fourier transform')
+plt.show()
